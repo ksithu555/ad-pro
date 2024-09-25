bc9f6c7f17aff8d32e9e1dafb7f112a9a3b714fa
ksithu555 <ksithu555@gmail.com>
2024-09-25 12:09:59 +0900

    header05

--- resources/views/one-page-advertisement.blade.php
+++ resources/views/one-page-advertisement.blade.php
@@ -610,6 +610,47 @@
                 </div><!-- END REVOLUTION SLIDER -->
             </section>
             <!--== Header04 End ==-->
+            @elseif ($advertisementSection->section->name == 'Header05')
+            <!--== Header05 Start ==-->
+            <section class="{{ $sectionBg }} pt-0 pb-0" id="{{ $advertisementSection->name }}">
+                @foreach ($advertisementSection->advertisementHeaderBlocks as $advertisementHeaderBlock)
+                @if ($advertisementHeaderBlock->status == 1)
+                <section class="pt-0 pb-0">
+                    <div class="parallax-overlay z-index-0"></div>
+                    <div class="container relative view-height-50vh">
+                        <div class="simple-content-slider text-center">
+                            <div class="simple-content-slider-text">
+                                <div class="simple-content-text-inner">
+                                    <div class="row">
+                                        <div class="col-md-10 centerize-col">
+                                            <div class="white-color text-center">
+                                                <span class="font-60px font-700 play-font wow fadeInUp font-italic" 
+                                                    data-wow-delay="0.1s">
+                                                    {!! nl2br($advertisementHeaderBlock->body) !!}
+                                                </span>
+                                                <h1 class="white-color font-700 font-120px line-height-120 xs-font-40px xs-line-height-50 sm-font-60px sm-line-height-60 wow fadeInUp" 
+                                                    data-wow-delay="0.2s">
+                                                    {{ $advertisementHeaderBlock->title }}
+                                                </h1>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="view-height-50vh absolute z-index-minus2 top-0 width-100-percent">
+                        <div class="view-height-50vh">
+                            <div class="slide-img parallax-bg fixed-bg" 
+                                data-parallax-bg-image="{{ asset('assets/images/all/' . $advertisementHeaderBlock->image) }}" 
+                                data-parallax-speed="0.8" data-parallax-direction="up"></div>
+                        </div>
+                    </div>
+                </section>
+                @endif
+                @endforeach
+            </section>
+            <!--== Header05 End ==-->
             @endif
 
         {{-- Header Hero End --}}
